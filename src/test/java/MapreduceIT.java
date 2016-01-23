746f4755b0b3e31f6cea51154c871aa652059bf7
Kostya Chyzhykau <kostianych@tut.by>
2016-01-24 02:06:53 +0300

    Initial commit

---
+++ src/test/java/MapreduceIT.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * Created by const on 1/24/16.
+ */
+public class MapreduceIT {
+
+    @Test
+    public void testMapReduce() {
+        System.out.print("Hello from test");
+
+    }
+
+}
